539839b26d490726e86a79c82a93244a9d766cd3
im-sanny <raselparvezsanny@gmail.com>
2024-05-12 09:50:10 +0600

    delete api created

--- index.js
+++ index.js
@@ -24,32 +24,57 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    const asnCollection = client.db('studyHive').collection('asnmnts')
-    const takeAsnCollection = client.db('studyHive').collection('takeAsnmnts')
+    const asnCollection = client.db("studyHive").collection("asnmnts");
+    const takeAsnCollection = client.db("studyHive").collection("takeAsnmnts");
 
     //get all assignment data from db
-    app.get('/asnmnts', async(req, res) => {
-        const result = await asnCollection.find().toArray()
-        res.send(result);
-    })
+    app.get("/asnmnts", async (req, res) => {
+      const result = await asnCollection.find().toArray();
+      res.send(result);
+    });
 
-    // get a single assignment data 
-    app.get('/asnmnt/:id', async(req, res) =>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await asnCollection.findOne(query)
-      res.send(result)
-    })
+    // get a single assignment data
+    app.get("/asnmnt/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await asnCollection.findOne(query);
+      res.send(result);
+    });
+
+    // get all assignment created by specific user
+    //later
+    // app.get('/asnmnts/:email', async(req, res) =>{
+    //   const email = req.params.email
+    //   const query = {'student.email': email}
+    //   const result = await asnCollection.find(query).toArray()
+    //   res.send(result)
+    // })
+
+    // save a create assignment in db
+    app.post("/asnmnts", async (req, res) => {
+      const asnData = req.body;
+      console.log(asnData);
+      const result = await asnCollection.insertOne(asnData);
+      res.send(result);
+    });
+
+    // delete a assignment from assignment
+    app.delete("/asnmnt/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await asnCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // save a take assignment in db
-    app.post('/takeAsnmnt', async(req,res)=>{
-      const takeData = req.body
+    app.post("/takeAsnmnt", async (req, res) => {
+      const takeData = req.body;
       console.log(takeData);
       // return
-      const result = await takeAsnCollection.insertOne(takeData)
-      res.send(result)
-    })
-    
+      const result = await takeAsnCollection.insertOne(takeData);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
